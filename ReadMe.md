7c0c784b81ed3fdd92145d63dfe4da08acafdcf9
orangeman <flo@andlabs.de>
2012-09-17 16:01:36 +0200

    documentation

--- ReadMe.md
+++ ReadMe.md
@@ -6,15 +6,61 @@
 
 ![http://andlabs.eu](http://andlabs.info/pix/android_andlabs_logo.png)
 
-### run
+### Usage
 
-    $ git clone git@github.com:ANDLABS-Git/studio-lounge-droid.git
-    $ android update project --path studio-lounge.droid
-    $ cd studio-lounge-droid && ant install
+  * include StudioLounge as [Android Library Project](http://developer.android.com/tools/projects/index.html)
+  
+  * register GCP background Service and LoungActivity in AndroidManifest.xml
+  ```Xml
+    <activity android:name="eu.andlabs.studiolounge.LoungeMainActivity" />
+    <service android:name="eu.andlabs.studiolounge.gcp.GCPService" />
+  ```
+  Themes can be applied to the Lounge Activity
 
+  * launch the Lounge Activity to visit the Player Lobby and Chat
 
+  ```Java
+    Lounge.startLoungeActivity(this);
+  ```
 
+  * add category "eu.andlabs.lounge" to the Game Intent Filter
+  ```Xml
+    <intent-filter>
+      <action android:name="android.intent.action.MAIN" />
+      <category android:name="eu.andlabs.lounge" />
+    </intent-filter>
+  ```
+  This activity will be started once players joined for a game
 
+  * start, bind and unbind the GCP backround service
+  ```Java
+    @Override
+    protected void onStart() { mLounge = new Lounge(this); }
 
+    @Override
+    protected void onStop() { unbindService(mLounge); }
+  ```
 
+  * send and receive game messages
+  ```Java
+    Bundle data = new Bundle();
+    data.putString("foo", "bar");
+    mLounge.sendGameMessage(data)
 
+    mLounge.register(new GameMsgListener() {                
+      @Override
+      public void onMessageRecieved(Bundle msg) {
+        msg.getString("foo") // returns "bar"
+      }
+    });
+  ```
+  Currently only String data is supported
+
+  * register listeners to implement a custom Lounge experience
+  ```Java
+    mLounge.register( new LobbyListener() {...} );
+    mLounge.register( new ChatListener() {...} );
+  ```
+
+### Sample Game
+  [Moving Points](https://github.com/ANDLABS-Git/points)
--- src/eu/andlabs/studiolounge/gcp/Lounge.java
+++ src/eu/andlabs/studiolounge/gcp/Lounge.java
@@ -15,7 +15,6 @@
  */
 package eu.andlabs.studiolounge.gcp;
 
-
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -32,69 +31,83 @@ import android.util.Log;
 
 public class Lounge implements ServiceConnection {
 
-	protected static final String TAG = "Lounge";
+    protected static final String TAG = "Lounge";
+
+    static public void startLoungeActivity(Context context) {
+        Intent i = new Intent();
+        i.setPackage("eu.andlabs.studiolounge");
+        i.setClassName(context, "eu.andlabs.studiolounge.LoungeMainActivity");
+        context.startActivity(i);
+    }
 
     public interface LobbyListener {
-        
+
         /**
          * is called after every successful login
-         * @param name  the player who has joined
+         * 
+         * @param name
+         *            the player who has joined
          */
         public void onPlayerLoggedIn(String player);
-        
+
         /**
          * is called after every logout operation
-         * @param name  the player who has left
+         * 
+         * @param name
+         *            the player who has left
          */
         public void onPlayerLeft(String player);
-        
+
         public void onNewHostedGame(String player, String Game);
-        
+
         public void onPlayerJoined(String player);
-        
+
     }
-    
+
     public interface ChatListener {
-        
+
         /**
          * is called when a chat message arrives
-         * @param text  the content of the message
+         * 
+         * @param text
+         *            the content of the message
          */
         public void onChatMessageRecieved(ChatMessage msg);
-        
+
     }
-    
+
     public interface GameMsgListener {
-        
+
         /**
          * is called when custom game messages come in
-         * @param msg  the content of the json message
+         * 
+         * @param msg
+         *            the content of the json message
          */
         public void onMessageRecieved(Bundle msg);
-        
+
     }
-    
+
     public static class ChatMessage {
         public String player;
         public String text;
     }
-
-
 
     private Vibrator mVibrator;
 
     public Lounge(Context context) {
         Log.d("Lounge", "Lounge Constructor");
         Intent intent = new Intent(context, GCPService.class);
-        intent.putExtra("packageName", context.getPackageName() );
+        intent.putExtra("packageName", context.getPackageName());
         intent.putExtra("messenger", mMessenger);
         context.startService(intent);
         context.bindService(intent, this, context.BIND_AUTO_CREATE);
-        mVibrator = (Vibrator)context.getSystemService(context.VIBRATOR_SERVICE);
+        mVibrator = (Vibrator) context
+                .getSystemService(context.VIBRATOR_SERVICE);
     }
 
     // receive incoming android system IPC messages from backround GCP service
-    final Messenger mMessenger = new Messenger(new Handler(){
+    final Messenger mMessenger = new Messenger(new Handler() {
 
         @Override
         public void handleMessage(Message msg) {
@@ -102,7 +115,8 @@ public class Lounge implements ServiceConnection {
             switch (msg.what) {
             case GCPService.LOGIN:
                 Log.d(TAG, "Lounge on LOGIN " + msg.obj);
-                if (mName == null) mName = (String) msg.obj;
+                if (mName == null)
+                    mName = (String) msg.obj;
                 if (mLobbyListener != null)
                     mLobbyListener.onPlayerLoggedIn(msg.obj.toString());
                 break;
@@ -119,8 +133,8 @@ public class Lounge implements ServiceConnection {
                 Log.d(TAG, "Lounge on HOST " + msg.obj);
                 if (mLobbyListener != null) {
                     Bundle b = (Bundle) msg.obj;
-                    mLobbyListener.onNewHostedGame(
-                            b.getString("host"), b.getString("game"));
+                    mLobbyListener.onNewHostedGame(b.getString("host"),
+                            b.getString("game"));
                 }
                 break;
             case GCPService.JOIN:
@@ -137,64 +151,84 @@ public class Lounge implements ServiceConnection {
             case GCPService.LEAVE:
                 mLobbyListener.onPlayerLeft(msg.obj.toString());
                 break;
-                
+
             }
-        }});
+        }
+    });
 
     private ChatListener mChatListener;
-    public void register(ChatListener listener) { mChatListener = listener; }
-    public void unregister(ChatListener listener) { mChatListener = null; }
-    
+
+    public void register(ChatListener listener) {
+        mChatListener = listener;
+    }
+
+    public void unregister(ChatListener listener) {
+        mChatListener = null;
+    }
+
     private LobbyListener mLobbyListener;
-    public void register(LobbyListener listener) { mLobbyListener = listener; }
-    public void unregister(LobbyListener listener) { mLobbyListener = null; }
+
+    public void register(LobbyListener listener) {
+        mLobbyListener = listener;
+    }
+
+    public void unregister(LobbyListener listener) {
+        mLobbyListener = null;
+    }
 
     private GameMsgListener mMsgListener;
-    public void register(GameMsgListener listener) { mMsgListener = listener; }
-    public void unregister(GameMsgListener listener) { mMsgListener = null; }
 
+    public void register(GameMsgListener listener) {
+        mMsgListener = listener;
+    }
+
+    public void unregister(GameMsgListener listener) {
+        mMsgListener = null;
+    }
 
     // send android system IPC message to backround GCP service
     private Messenger mService;
 
     private String mName;
-    
+
     @Override
     public void onServiceConnected(ComponentName name, IBinder service) {
-        Log.d("Lounge", "Service Connected "+service);
+        Log.d("Lounge", "Service Connected " + service);
         mService = new Messenger(service);
         sendMessage(GCPService.REGISTER, mMessenger);
     }
-    
+
     private void sendMessage(int what, Object thing) {
         try {
             mService.send(Message.obtain(null, what, thing));
-        } catch (RemoteException e) { e.printStackTrace(); }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public void sendChatMessage(ChatMessage msg) {
         sendMessage(GCPService.CHAT, msg.text);
     }
-    
+
     public void hostGame() {
         sendMessage(GCPService.HOST, null);
     }
-    
+
     public void joinGame(String hostplayer, String gamepackage) {
         Bundle b = new Bundle();
         b.putString("host", hostplayer);
         b.putString("game", gamepackage);
         sendMessage(GCPService.JOIN, b);
     }
-    
+
     public void sendGameMessage(Bundle msg) {
         sendMessage(GCPService.CUSTOM, msg);
     }
-    
+
     public String getName() {
         return mName;
     }
-    
+
     @Override
     public void onServiceDisconnected(ComponentName name) {
         Log.d("Lounge", "Service DISconnected");
